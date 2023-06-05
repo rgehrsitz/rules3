89fc583fd750c56973396a60f43145c12cedfb04
robertg <36572841+rgehrsitz@users.noreply.github.com>
2023-06-04 20:26:53 -0400

    Adding new scenarios

--- main.go
+++ main.go
@@ -9,39 +9,41 @@ import (
 
 func main() {
 
-	jsonRule1, err := os.ReadFile("rule1.json")
-	if err != nil {
-		panic(err)
-	}
-	// jsonRule2, err := os.ReadFile("rule2.json")
+	// jsonRule1, err := os.ReadFile("rule1.json")
 	// if err != nil {
 	// 	panic(err)
 	// }
+	jsonRule2, err := os.ReadFile("rule2.json")
+	if err != nil {
+		panic(err)
+	}
 
 	// Define the data to be used in the conditions
 	data1 := map[string]interface{}{
 		"Sensor1Temp": 9,
-		"Sensor2Temp": 9,
+		"Sensor2Temp": 999,
+		"Sensor3Temp": 100,
 	}
 
-	// data2 := map[string]interface{}{
-	// 	"sensor2temp":     177,
-	// 	"sensor2pressure": 50,
-	// }
+	data2 := map[string]interface{}{
+		"Sensor2Temp":     177,
+		"Sensor2Pressure": 60,
+	}
 
 	engine := ruleEngine.New(&ruleEngine.EvaluatorOptions{
 		AllowUndefinedVars: true,
 	})
 
-	engine.AddRule(string(jsonRule1))
+	// engine.AddRule(string(jsonRule1))
+	engine.AddRule(string(jsonRule2))
 	// engine.AddRules(string(jsonRule1), string(jsonRule2))
 
 	// Evaluate the conditions using the EvaluateCondition function
 	result1 := engine.EvaluateRules(data1)
-	// result2 := engine.EvaluateRules(data2)
+	result2 := engine.EvaluateRules(data2)
 
 	// Print the result
 	fmt.Println(result1)
-	// fmt.Println(result2)
+	fmt.Println(result2)
 
 }
